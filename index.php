62976b920d024464d1af9264098c74cce8d644b6
ChrisJosJC <63082513+ChrisJosJC@users.noreply.github.com>
2023-05-18 22:55:02 -0400

    Driving errors

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
+error_reporting(E_ALL); // Error/Exception engine, always use E_ALL
+
+ini_set('ignore_repeated_errors', TRUE); // always use TRUE
+
+ini_set('display_errors', TRUE); // Error/Exception display, use FALSE only in production environment or real server. Use TRUE in development environment
+
+ini_set('log_errors', TRUE); // Error/Exception file logging engine.
+
+ini_set("error_log", "error.log");
+error_log("Hello, errors!");
+
 require_once 'libs/database.php';
 require_once 'libs/controller.php';
 require_once 'libs/view.php';
