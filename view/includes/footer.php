fd5b1231cd84178ea9055ea16e3c993e5f74b8b4
ChrisJosJC <63082513+ChrisJosJC@users.noreply.github.com>
2023-05-31 20:47:18 -0400

    Target footer!

--- view/includes/footer.php
+++ view/includes/footer.php
@@ -5,16 +5,16 @@
                     <h4>Piniors</h4>
                     <ul>
                         <li><a href="/">Pagina principal</a></li>
-                        <li><a href="/public/politicas.pdf">Politicas de privacidad</a></li>
+                        <li><a target="_blank" href="/public/politicas.pdf">Politicas de privacidad</a></li>
                     </ul>
                 </div>
                 <div class="footer-col">
                     <h4>Nuestras fuentes</h4>
                     <ul>
-                        <li><a href="https://github.com">Github</a></li>
-                        <li><a href="https://developer.mozilla.org/en-US/">MDN Web Docs</a></li>
-                        <li><a href="https://www.w3schools.com/default.asp">W3C</a></li>
-                        <li><a href="https://css-tricks.com/">CSS Tricks</a></li>
+                        <li><a target="_blank" href="https://github.com">Github</a></li>
+                        <li><a target="_blank" href="https://developer.mozilla.org/en-US/">MDN Web Docs</a></li>
+                        <li><a target="_blank" href="https://www.w3schools.com/default.asp">W3C</a></li>
+                        <li><a target="_blank" href="https://css-tricks.com/">CSS Tricks</a></li>
                     </ul>
                 </div>
                 <div class="footer-col">
@@ -22,8 +22,8 @@
                 <div class="footer-col">
                     <h4>Siguenos</h4>
                     <div class="social-links">
-                        <a href="https://www.facebook.com/profile.php?id=100093116745063"><i class="fab fa-facebook-f"></i></a>
-                        <a href="https://instagram.com/piniorsdev"><i class="fab fa-instagram"></i></a>
+                        <a target="_blank" href="https://www.facebook.com/profile.php?id=100093116745063"><i class="fab fa-facebook-f"></i></a>
+                        <a target="_blank" href="https://instagram.com/piniorsdev"><i class="fab fa-instagram"></i></a>
                     </div>
                 </div>
             </div>
